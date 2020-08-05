d3748cc91d01d1daf4a35b1bcab12436e63ece0a
Adia Mahardika <dikaadia@gmail.com>
2020-08-05 19:17:37 +0700

    insert and read ads table

--- src/components/dashboard/ads/ads.js
+++ src/components/dashboard/ads/ads.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import Sidebar from "../layout/sidebar";
 import Navbar from "../layout/navbar";
 import { withRouter } from "react-router";
@@ -6,6 +6,7 @@ import { connect } from "react-redux";
 import "../../css/dashboard/ads.css";
 import "../../css/dashboard/layout.css";
 import { readAds } from "../../redux/action/ads";
+import InsertAds from "./insert_ads";
 class DashboardAds extends Component {
   componentDidMount() {
     this.readAds();
@@ -13,13 +14,13 @@ class DashboardAds extends Component {
   readAds() {
     this.props.dispatch(readAds());
   }
-  getDateTimeFromTimestamp(unixTimeStamp) {
-    let date = new Date(unixTimeStamp);
+  parseDate(time) {
+    let date = new Date(time);
     return (
       ("0" + date.getDate()).slice(-2) +
-      "/" +
+      "-" +
       ("0" + (date.getMonth() + 1)).slice(-2) +
-      "/" +
+      "-" +
       date.getFullYear() +
       " " +
       ("0" + date.getHours()).slice(-2) +
@@ -32,56 +33,67 @@ class DashboardAds extends Component {
     const listAds =
       ads &&
       ads.map((ads, index) => (
-        <>
+        <Fragment key={ads.id}>
           <div>{index + 1}</div>
           <div className="manage-ads">
-            <button type="button" className="btn btn-sm btn-outline-delete">Delete</button>
-            <button type="button" className="btn btn-sm btn-edit">Edit</button>
+            <button type="button" className="btn btn-sm btn-outline-delete">
+              Delete
+            </button>
+            <button type="button" className="btn btn-sm btn-edit">
+              Edit
+            </button>
           </div>
           <div className="name-ads">{ads.ads_name}</div>
           <div>
             <img
-            width={400} height={150}
+              width={400}
+              height={150}
               src={ads.ads_image}
               alt="..."
               style={{ maxHeight: 50 }}
             />
           </div>
-          <div>{this.getDateTimeFromTimestamp(ads.date_created)}</div>
-          <div>{this.getDateTimeFromTimestamp(ads.date_updated)}</div>
-        </>
+          <div>{this.parseDate(ads.date_created)}</div>
+          <div>{this.parseDate(ads.date_updated)}</div>
+        </Fragment>
       ));
     return (
       <div className="container">
         <Sidebar />
         <Navbar />
         <div className="container-ads">
-          <div className="title-ads">Ads</div>
+          <div className="title-ads">Ads Table</div>
           <form className="form ads">
-          <button type="button" className="btn btn-sm btn-add">Add</button>
+            <button
+              type="button"
+              className="btn btn-sm btn-add"
+              data-toggle="modal"
+              data-target="#modalInsertAds"
+            >
+              Add
+            </button>
             <input
               className="form-control ads"
               type="search"
               placeholder="Search Ads"
             />
-            
           </form>
           <div className="table-ads">
-              <div className='header-table-ads'>No</div>
-              <div className='header-table-ads'>Manage</div>
-              <div className='header-table-ads'>Name</div>
-              <div className='header-table-ads'>Image</div>
-              <div className='header-table-ads'>Date Created</div>
-              <div className='header-table-ads'>Date Updated</div>
-              {listAds}
+            <div className="header-table-ads">No</div>
+            <div className="header-table-ads">Manage</div>
+            <div className="header-table-ads">Name</div>
+            <div className="header-table-ads">Image</div>
+            <div className="header-table-ads">Date Created</div>
+            <div className="header-table-ads">Date Updated</div>
+            {listAds}
           </div>
         </div>
+        <InsertAds />
       </div>
     );
   }
 }
 const mapStateToProps = (state) => {
-  console.log(state)
   return {
     ads: state.ads.ads,
   };
---
+++ src/components/dashboard/ads/insert_ads.js
@@ -0,0 +1,117 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { insertAds } from "../../redux/action/ads";
+import { withRouter } from "react-router-dom";
+class InsertAds extends Component {
+  state = {
+    ads_name: "",
+    ads_image: "",
+  };
+
+  onInsertAds = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+  onInsertImage = event => {
+    const image = event.target.files[0]
+      if (image.size > 1024 * 1024 * 2)
+      return alert('Cannot upload image with size more than 2mb')
+      const imageArray = image.name.split(".");
+      const imageExtension = imageArray[imageArray.length - 1].toLowerCase();
+      if (
+      imageExtension !== "png" &&
+      imageExtension !== "jpg" &&
+      imageExtension !== "jpeg"
+      )
+      return alert("Cannot upload file except image!")
+      this.setState({
+          ads_image:image
+      })
+  }
+  insertAds = async (event) => {
+    event.preventDefault();
+    let data = new FormData();
+
+    data.append("ads_name", this.state.ads_name);
+    data.append("ads_image", this.state.ads_image);
+
+    await this.props.dispatch(insertAds(data));
+    this.props.history.push("/ads");
+  };
+  render() {
+    return (
+      <>
+        <div
+          className="modal fade"
+          id="modalInsertAds"
+          role="dialog"
+          aria-labelledby="modalInsertAdsTitle"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog modal-dialog-centered" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="modalInsertAdsTitle">
+                  Insert News Ads
+                </h5>
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <form className="needs-validation" noValidate>
+                  <div className="form-group">
+                    <div>
+                      <label htmlFor="validationCustom01" className="col-form-label">
+                        Ads Name:{" "}
+                      </label>
+                      <input
+                        name="ads_name"
+                        type="text"
+                        className="form-control"
+                        id="validationCustom01"
+                        onChange={this.onInsertAds}
+                        required
+                      />
+                      <div className="valid-feedback">Looks good!</div>
+                    </div>
+                    <div>
+                      <label className="col-form-label"> Image: </label>
+                      <input
+                        type="file"
+                        name="ads_image"
+                        className="form-control"
+                        onChange={this.onInsertImage}
+                        required
+                      />
+                    </div>
+                  </div>
+                </form>
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button type="submit" className="btn btn-primary" onClick={this.insertAds} data-dismiss="modal">
+                  Add
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default withRouter(connect()(InsertAds));
