4e19180e34cddedb13a4dcb5abef11fe5bbd3e2a
Adia Mahardika <dikaadia@gmail.com>
2020-08-11 10:39:59 +0700

    rename folder dashbord to admin

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import { routes } from "./components/helpers/routes.json";
 import store from "./components/redux/store";
-import ads from "./components/dashboard/ads/ads";
-import news_category from "./components/dashboard/news_category/news_category";
-import user_role from "./components/dashboard/user_role/user_role";
-import video_category from "./components/dashboard/video_category/video_category";
+import ads from "./components/admin/ads/ads";
+import news_category from "./components/admin/news_category/news_category";
+import user_role from "./components/admin/user_role/user_role";
+import video_category from "./components/admin/video_category/video_category";
 export default function App() {
   return (
     <Provider store={store}>
