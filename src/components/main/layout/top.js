7a60496ab6fef960205de42bf2e666a0c8261b4b
Adia Mahardika <dikaadia@gmail.com>
2020-08-25 13:32:35 +0700

    edit top component

--- src/components/main/layout/top.js
+++ src/components/main/layout/top.js
@@ -2,45 +2,60 @@ import React, { Component } from "react";
 import { withRouter } from "react-router";
 import { connect } from "react-redux";
 import { readAds } from "../../redux/action/ads";
-import lensajabar from "../../../assets/image/logo/lensajabar.png"
-import youtube from "../../../assets/image/logo/youtube.jpg"
-import instagram from "../../../assets/image/logo/instagram.jpg"
-import twitter from "../../../assets/image/logo/twitter.png"
-import facebook from "../../../assets/image/logo/facebook.png"
-import "../../css/main/layout/top.css"
-import "../../css/components/form.css"
-import "../../css/components/media.css"
+import lensajabar from "../../../assets/image/logo/lensajabar.png";
+import youtube from "../../../assets/image/logo/youtube.jpg";
+import instagram from "../../../assets/image/logo/instagram.jpg";
+import twitter from "../../../assets/image/logo/twitter.png";
+import facebook from "../../../assets/image/logo/facebook.png";
+import "../../css/main/layout/top.css";
+import "../../css/components/form.css";
+import "../../css/components/media.css";
 class Top extends Component {
   componentDidMount() {
     this.props.dispatch(readAds());
   }
   render() {
+    const { ads } = this.props;
     return (
-      <div className="top">
-        <img alt="" />
-        <div className="top-nav top">
-          <input
-            className="form-control search"
-            type="search"
-            placeholder="Search News"
-          />
-          Connect with us
-          <img className="socmed-logo" alt="" src={youtube}/>
-          <img className="socmed-logo" alt="" src={instagram}/>
-          <img className="socmed-logo" alt="" src={twitter}/>
-          <img className="socmed-logo" alt="" src={facebook}/>
-          <button type="button" className="btn primary">
-            Daftar Lensa ID
-          </button>
-          <button type="button" className="btn secondary">
-            Masuk
-          </button>
+      <div className="top-section">
+        {ads.map((item, index) => {
+          if (index === 0) {
+            return <img src={item.ads_image} alt="" />;
+          }
+        })}
+        <div className="top-nav">
+          <div className="top">
+            <input
+              className="form-control search"
+              type="search"
+              placeholder="Search News"
+            />
+            Connect with us
+            <img className="socmed-logo" alt="" src={youtube} />
+            <img className="socmed-logo" alt="" src={instagram} />
+            <img className="socmed-logo" alt="" src={twitter} />
+            <img className="socmed-logo" alt="" src={facebook} />
+            <button type="button" className="btn primary">
+              Daftar Lensa ID
+            </button>
+            <button type="button" className="btn secondary">
+              Masuk
+            </button>
+          </div>
+          <div className="bottom">
+            <img className="lensajabar-logo" src={lensajabar} alt="" />
+            {ads.map((item, index) => {
+              if (index === 1) {
+                return <img src={item.ads_image} alt="" />;
+              }
+            })}
+          </div>
         </div>
-        <div className="top-nav bottom">
-          <img className="lensajabar-logo" src={lensajabar} alt=""/>
-          <img alt="" />
-        </div>
-        <img alt="" />
+        {ads.map((item, index) => {
+          if (index === 2) {
+            return <img src={item.ads_image} alt="" />;
+          }
+        })}
       </div>
     );
   }
