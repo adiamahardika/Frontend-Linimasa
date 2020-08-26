f265d775bd90c1e1f5480226eed3da7cd9dcd240
Adia Mahardika <dikaadia@gmail.com>
2020-08-26 14:16:41 +0700

    refactor home.js

--- src/components/main/home.js
+++ src/components/main/home.js
@@ -42,6 +42,7 @@ class Home extends Component {
                   />
                 );
               }
+              return null
             })}
             <div>
               <div className="home-news-header">
